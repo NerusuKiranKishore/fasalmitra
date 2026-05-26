521c3396528ffe346be86610ebe7463b300fdcc7
Nerusu Kiran Kishore <kirankishorenerusu@gmail.com>
2026-05-26 15:48:27 +0530

    Solved an error.
    The animated wave SVG canvas are not properly working in mobile devise, so I made sure it is also working in moblies and smartphones.

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-// Animated wave SVG canvas
 function WaveCanvas() {
   const canvasRef = useRef(null);
   useEffect(() => {
